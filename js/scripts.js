086b624e4775cb7fec698b828d94ff925c1d921f
ryotaka18 <hurleyx.hp.81rt@outlook.jp>
2023-05-01 13:05:43 +0900

    worksアニメーション化

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const container = document.querySelector('.portfolio-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+  isDown = true;
+  container.classList.add('active');
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+  isDown = false;
+  container.classList.remove('active');
+});
+
+container.addEventListener('mouseup', () => {
+  isDown = false;
+  container.classList.remove('active');
+});
+
+container.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 3;
+  container.scrollLeft = scrollLeft - walk;
+});
+
